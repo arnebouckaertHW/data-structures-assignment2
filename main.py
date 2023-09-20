c18ed4f7feaa47f7143ff3d28c256cff4720245c
arnebouckaertHW <arne.bouckaert@student.howest.be>
2023-09-20 09:43:57 -0400

    Make assignment

---
+++ main.py
@@ -0,0 +1,31 @@
+from serialsearch import *
+
+list1 = ['E', 'B', 'E', 'F', 'A', 'F']
+list2 = ['E', 'B', 'E', 'F', 'A', 'F']
+list3 = ['E', 'B', 'E', 'F', 'A', 'C']
+list4 = ['E', 'B', 'E', 'F', 'A', 'F', 'C']
+
+def main(list1, list2):
+    if(len(list1) != len(list2)):
+        print("Two lists aren't strictly identical!")
+        print(list1)
+        print(list2)
+        return
+    
+    for i in range(len(list1)):
+        if(search(list2, i, 1, list1[i]) == -1):
+            print("Two lists aren't strictly identical!")
+            print(list1)
+            print(list2)
+            return
+        else:
+            i += 1
+    
+    print("Two lists are strictly identical!")
+    print(list1)
+    print(list2)
+
+if __name__ == "__main__":
+    main(list1, list2)
+    main(list3, list1)
+    main(list4, list1)
